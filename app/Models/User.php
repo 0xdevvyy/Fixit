d4a8f8039da1c84cdaf2f131e8ebfff876dbe996
0xdevvyy <pullhead45@gmail.com>
2026-06-28 21:46:35 +0800

    feat(database): added a building migration/table

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
@@ -47,7 +48,16 @@ class User extends Authenticatable
 
 
     public function reportedTickets(): HasMany{
-        return $this->hasMany(Ticket::class);
+        return $this->hasMany(Ticket::class, 'user_id');
+    }
+
+    public function assignedTickets(): HasMany {
+        return $this->hasMany(Ticket::class, 'assigned_to');
+    }
+
+
+    public function building(): HasOne {
+        return $this->hasOne(Building::class); //a building is assigned to a user,
     }
 
 
--- database/migrations/2026_06_27_115525_create_tickets_table.php
+++ database/migrations/2026_06_27_115525_create_tickets_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Building;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -16,8 +17,9 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete(); //user_id, reporter(Facilitator, teacher)
             $table->foreignIdFor(User::class, 'assigned_to')->nullable()->constrained()->cascadeOnDelete(); //assigned_to, Maintenance
+            $table->foreignIdFor(Building::class)->constrained()->cascadeOnDelete(); //building_id
             $table->string('ticket_number')->unique();
-            $table->string('location')->nullable();
+            $table->string('room')->nullable();
             $table->string('category')->default('aircon')->nullable();
             $table->string('description');
             $table->string('priority')->default('medium')->nullable();
